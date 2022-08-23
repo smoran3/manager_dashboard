e0a6d9f0a92139917137fd170d871f5f1f9dc30a
smoran3 <smoran@dvrpc.org>
2022-08-23 16:14:27 -0400

    add axis title

--- chart.js
+++ chart.js
@@ -87,6 +87,18 @@ const config = {
       x: {
         min: 0,
         max: 100,
+        title: {
+          display: true,
+          text: "Percent Spent",
+          color: "#0078ae",
+          font: {
+            family: "Arial",
+            size: 14,
+            style: "normal",
+            lineHeight: 1.2,
+          },
+          padding: { top: 10, left: 0, right: 0, bottom: 0 },
+        },
       },
     },
   },
