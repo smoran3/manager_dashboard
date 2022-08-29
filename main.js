a7d11044e6a2590bc53a490004c30d20a406770e
smoran3 <smoran@dvrpc.org>
2022-08-29 13:48:57 -0400

    trying to import list

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-//let array = [19033, 20121, 20037, 18041];
-//let start_url = "https://www2.dvrpc.org/api/pubs/id/";
-let array = ["23-52-030", "23-52-150", "23-52-120", "23-52-140"];
+import { id, pro_ids, pro_names } from "./sample_list";
+let array = pro_ids;
+//let array = ["23-52-030", "23-52-150", "23-52-120", "23-52-140"];
 let wp_url = "https://dvrpc-linuxdev.dvrpc.org/wp/2023/pm-projects/";
 let update_url = "https://dvrpc-linuxdev.dvrpc.org/wp/2023/pm-projectupdates/";
 
@@ -16,9 +16,7 @@ async function getData(url) {
 async function render() {
   let html = "";
   for (var i in array) {
-    //let i_url = wp_url.concat(array[i]);
     let u_url = update_url.concat(array[i]);
-    //let i = await getData(i_url);
     let u = await getData(u_url);
     let htmlSegment = `<div class = "test"> 
     <p>Project Code: ${u.proid} </br> Month: ${u.month1}</br> Monthly Report: ${u.notes}</p> 
@@ -31,33 +29,3 @@ async function render() {
   }
 }
 render();
-
-//render();
-/*
-async function getPublication(url) {
-  try {
-    let res = await fetch(url);
-    return await res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function renderPublications() {
-  let html = "";
-  for (var i in array) {
-    let url = start_url.concat(array[i]);
-    let pub = await getPublication(url);
-    let htmlSegment = `<div class = "test"> 
-                <p>Publication ID: ${pub.PubId} </br> Title: ${pub.Title}</br> Staff Contact: ${pub.StaffContactName}</p> 
-                <p> Abstract: ${pub.Abstract}</p>
-                </div>`;
-
-    html += htmlSegment;
-  }
-  let container = document.querySelector(".container");
-  container.innerHTML = html;
-}
-
-renderPublications();
-*/
---
+++ sample_list.js
@@ -0,0 +1,8 @@
+export const id = "23-52-030";
+export const pro_ids = ["23-52-030", "23-52-150", "23-52-120", "23-52-140"];
+export const pro_names = [
+  "Mobility Analysis and Design Program",
+  "PennDOT Connects Bike Friendly Resurfacing Program",
+  "PennDOT Road Diet Prioritization",
+  "Pottstown Area Regional Plan Development",
+];
